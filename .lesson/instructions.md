753a2f00e21edf55657accf964771a84b00951a8
mikey788 <97480161+mikey788@users.noreply.github.com>
2024-01-01 21:01:30 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,14 @@
+# Instructions  
+Instructions: This program reads 3 numbers and prints their sum. Leave the prompt blank i.e. input()
+
+---
+Input:\
+3\
+2\
+1
+
+Output:\
+6
+
+  
+  
---
+++ main.py
@@ -0,0 +1,5 @@
+x1 = int(input(" Please enter a number "));
+x2 = int(input(" Please enter a second number"));
+x3 = int(input ("Please enter a third number"));
+x4 = x1 + x2 + x3;
+print ( x4 );
